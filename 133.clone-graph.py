ee86e6d76131d258c24bf7021963855f6aeb8b06
smallcat9603 <huyao0107@gmail.com>
2022-11-13 23:12:18 +0900

    minor

---
+++ 133.clone-graph.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=133 lang=python3
+#
+# [133] Clone Graph
+#
+
+# @lc code=start
+
+import copy
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        return copy.deepcopy(node)
+        
+# @lc code=end
+
